119dbfa9be516a4eccc7841fec5831d8f96e1b7b
m-riss <95452643+m-riss@users.noreply.github.com>
2023-09-13 18:15:44 -0400

    Update Holy_Bible.cpp

--- Holy_Bible.cpp
+++ Holy_Bible.cpp
@@ -8,12 +8,18 @@ using std::string;
 
 int main() {
 int Exit_Variable = 0;
+int Number_of_Bytes = 0;
+int Number_of_Bytes_Per_Binary_Kilobyte = 0;
+int Number_of_Kilobytes = 0;
+
+string Exclamation_Point = "";
 string QuestionMark = "";
 string Period = "";
 string Space = "";
 string Comma = "";
 string Colon = "";
 string Hyphen = "";
+string Semicolon = "";
 string day = "";
 string days = "";
 string night = "";
@@ -21,8 +27,14 @@ string seas = "";
 string land = "";
 string sky = "";
 string Exit_Variable_Text = "";
+string livestock = "";
+string look = "";
 
+Number_of_Bytes_Per_Binary_Kilobyte = 1024;
+
+livestock = "livestock";
 night = "night";
+look = "Look";
 days = "days";
 seas = "seas";
 land = "land";
@@ -35,10 +47,19 @@ Period = ".";
 Comma = ",";
 Space = " ";
 QuestionMark = "?";
+Semicolon = ";";
+Exclamation_Point = "!";
+
+//cout << "What is the Number of Bytes Without Optimization" << QuestionMark << endl;
+//cin >> Number_of_Bytes;
+//Number_of_Kilobytes = (Number_of_Bytes / Number_of_Bytes_Per_Binary_Kilobyte);
+//cout << "Without Optimization" << Comma << Space << "The Number of Kilobytes is" << Colon << Space << Number_of_Kilobytes << endl;
 
 cout << "Genesis 1" << Colon << endl;
 cout << "The Account of Creation" << Period << endl;
 
+cout << Space << endl;
+cout << "Day One" << Colon << endl;
 cout << "In the beginning God created the heavens and the earth" << Period << endl;
 cout << "The earth was formless and empty" << Comma << Space << "and darkness covered the deep waters" << Period << endl;
 cout << "And the Spirit of God was hovering over the surface of the waters" << Period << endl;
@@ -48,12 +69,16 @@ cout << "Then he separated the light from the darkness" << Period << endl;
 cout << "God called the light" << Space << day << Space << "and the darkness" << Space << night << Period << endl;
 cout << "And evening passed and morning came" << Comma << Space << "marking the first day" << Period << endl;
 
+cout << Space << endl;
+cout << "Day Two" << Colon << endl;
 cout << "Then God said" << Comma << Space << "Let there be a space between the waters" << Comma << Space << "to separate the waters of the heavens from the waters of the earth" << Period << endl;
 cout << "And that is what happened" << Period << endl;
 cout << "God made this space to separate the waters of the earth from the waters of the heavens" << Period << endl;
 cout << "God called the space" << Space << sky << Period << endl;
 cout << "And evening passed and morning came" << Comma << Space << "marking the second day" << Period << endl;
 
+cout << Space << endl;
+cout << "Day Three" << Colon << endl;
 cout << "Then God said" << Comma << Space << "Let the waters beneath the sky flow together into one place" << Comma << Space << "so dry ground may appear" << Period << endl;
 cout << "And that is what happened" << Period << endl;
 cout << "God called the dry ground" << Space << land << Space << "and the waters" << Space << seas << Period << endl;
@@ -66,6 +91,9 @@ cout << "Their seeds produced plants and trees of the same kind" << Period << en
 cout << "And God saw that it was good" << Period << endl;
 cout << "And evening passed and morning came" << Comma << Space << "marking the third day" << Period << endl;
 
+cout << Space << endl;
+cout << "Day Four" << Colon << endl;
+
 cout << "Then God said" << Comma << Space << "Let lights appear in the sky to separate the day from the night" << Period << endl;
 cout << "Let them be signs to mark the seasons" << Comma << Space << days << Comma << Space << "and years" << Period << endl;
 cout << "Let these lights in the sky shine down on the earth" << Period << endl;
@@ -76,6 +104,9 @@ cout << "God set these lights in the sky to light the earth" << Comma << Space <
 cout << "And God saw that it was good" << Period << endl;
 cout << "And evening passed and morning came" << Comma << Space << "marking the fourth day" << Period << endl;
 
+cout << Space << endl;
+cout << "Day Five" << Colon << endl;
+
 cout << "Then God said" << Comma << Space << "Let the waters swarm with fish and other life" << Period << endl;
 cout << "Let the skies be filled with birds of every kind" << Period << endl;
 cout << "So God created great sea creatures and every living thing that scurries and swarms in the water" << Comma << Space << "and every sort of bird" << Comma << Space << "each producing offspring of the same kind" << Period << endl;
@@ -84,9 +115,26 @@ cout << "Then God blessed them" << Comma << Space << "saying" << Comma << Space 
 cout << "Let the fish fill the seas" << Comma << Space << "and let the birds multiply on the earth" << Period << endl;
 cout << "And evening passed and morning came" << Comma << Space << "marking the fifth day" << Period << endl;
 
+cout << Space << endl;
+cout << "Day Six" << Colon << endl;
 
-
-
+cout << "Then God said" << Comma << Space << "Let the earth produce every sort of animal" << Comma << Space << "each producing offspring of the same kind" << Comma << Space << livestock << Comma << Space << "small animals that scurry along the ground" << Comma << Space << "and wild animals" << Period << endl;
+cout << "And that is what happened" << Period << endl;
+cout << "God made all sorts of wild animals" << Comma << Space << livestock << Comma << Space << "and small animals" << Comma << Space << "each able to produce offspring of the same kind" << Period << endl;
+cout << "And God saw that it was good" << Period << endl;
+cout << "Then God said" << Comma << Space << "Let us make human beings in our image" << Comma << Space << "to be like us" << Period << endl;
+cout << "They will reign over the fish in the sea" << Comma << Space << "the birds in the sky" << Comma << Space << "the livestock" << Comma << Space << "all the wild animals on the earth" << Comma << Space << "and the small animals that scurry along the ground" << Period << endl;
+cout << "So God created human beings in his own image" << Period << endl;
+cout << "In the image of God he created them" << Semicolon << endl;
+cout << "male and female he created them" << Period << endl;
+cout << "Then God blessed them and said" << Comma << Space << "Be fruitful and multiply" << Period << endl;
+cout << "Fill the earth and govern it" << Period << endl;
+cout << "Reign over the fish in the sea" << Comma << Space << "the birds in the sky" << Comma << Space << "and all the animals that scurry along the ground" << Period << endl;
+cout << "Then God said" << Comma << Space << look << Exclamation_Point << Space << "I have given you every seed" << Hyphen << "bearing plant throughout the earth and all the fruit trees for your food" << Period << endl;
+cout << "And I have given every green plant as food for all the wild animals" << Comma << Space << "the birds in the sky" << Comma << Space << "and the small animals that scurry along the ground" << Comma << Space << "everything that has life" << Period << endl;
+cout << "And that is what happened" << Period << endl;
+cout << "Then God looked over all he had made" << Comma << Space << "and he saw that it was very good" << Exclamation_Point << endl;
+cout << "And evening passed and morning came" << Comma << Space << "marking the sixth day" << Period << endl;
 
 
 
@@ -96,6 +144,9 @@ cout << "And evening passed and morning came" << Comma << Space << "marking the 
 
 
 /*
+
+
+
 
 
 */
