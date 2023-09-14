96b96b796390b9cbccddb18482b19de417ab0c26
m-riss <95452643+m-riss@users.noreply.github.com>
2023-09-14 17:45:00 -0400

    Create Strings.cpp

---
+++ Strings.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+string Exclamation_Point = "";
+string QuestionMark = "";
+string Period = "";
+string Space = "";
+string Comma = "";
+string Colon = "";
+string Hyphen = "";
+string Semicolon = "";
+string day = "";
+string days = "";
+string night = "";
+string seas = "";
+string land = "";
+string sky = "";
+string Exit_Variable_Text = "";
+string livestock = "";
+string look = "";
+string Read_The_Bible_Yes_or_No = "";
+string Apostrophe = "";
+string it = "";
+string s = "";
+string t = "";
+string die = "";
+string too = "";
+string that = "";
+string adam = "";
